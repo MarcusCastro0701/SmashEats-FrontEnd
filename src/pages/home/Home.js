dea52dc4710bae8348feb6627f3138a766342753
MarcusCastro0701 <marcus.castro0701@gmail.com>
2024-01-21 16:05:17 -0300

    fix: component import (OrdersDashboard)

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from '../../components/Header';
-import OrdersDashboard from '../../components/orders-dashboard/OrdersDashboard';
+import OrdersDashboard from '../../components/orders-dashboard/Dashboard';
 
 export default function Home() {
   return (
