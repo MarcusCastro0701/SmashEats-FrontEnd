90ff657bebefd954ca9af89f676644210eba9a83
MarcusCastro0701 <marcus.castro0701@gmail.com>
2024-01-21 22:08:34 -0300

    fix: console statement removed

--- src/components/orders-dashboard/products/ProductOrder.js
+++ src/components/orders-dashboard/products/ProductOrder.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export default function ProductOrder({ productOrder, quantity }) {
-  console.log(productOrder);
   return productOrder.map(p => (
     <div>
       {p.type === 1 ? <h2>{`${quantity}x ${p.name}`}</h2> : <h2>{p.name}</h2>}
